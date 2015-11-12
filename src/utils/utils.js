86dd878785b364ce75a57252674abe16bde62f0a
Mikolaj Dadela <mikolaj.dadela@hgv-online.de>
2015-11-12 14:08:26 +0100

    Workaround for non-working component name detection.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,11 @@ export function setComponentsNames(components) {
 	components.map((component) => {
 		let module = component.module;
 		component.name = module.displayName || module.name;
+		// "ProxyClass" is here because of https://github.com/gaearon/babel-plugin-react-transform/issues/19
+		if (component.name === 'ProxyClass') {
+			var m = /.*\/([^\/]+)\/index\.jsx?/.exec(component.filepath);
+			component.name = m[1]; // HACK HACK HACK
+		}
 		if (!component.name) {
 			throw Error(`Cannot detect component name for ${component.filepath}`);
 		}
